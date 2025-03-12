82a72d79fbecb5902c9dad4c7ad3e68ce9e1182f
Chars61 <c.marchese.dv@gmail.com>
2025-03-12 17:58:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // You can add as many as you like here.
 const openers = [
   "Hey, I love your shoes. Where did you get them?",
-  "Do you live in thie city? What's fun to do around here?",
+  "Do you live in the city? What's fun to do around here?",
   "If you could travel anywhere right now, where would you go?",
-  "I'm trying to choose my next read, do you have any recs?",
-  "I'm trying to choose my next movie, do you have any recs?",
+  "I'm trying to choose my next read. Do you have any recs?",
+  "I'm trying to choose my next movie. Do you have any recs?",
   "What's one interesting fact about you that most people don't know?",
   "Hey, I've never been here before—what's good on the menu?",
   "I noticed they're playing some great music tonight. Are you into this kind of stuff?",
@@ -15,7 +15,7 @@ const openers = [
   "I noticed you're reading the cocktail list like you're on a mission—any hidden gems I should know about?",
   "I'm in a celebratory mood. What's something good that happened to you recently?",
   "I love the energy in here. Do you come here often or is this just a spontaneous drop-in?",
-  "I couldn't help but overhear your comment about [topic]. Mind sharing more about that?"
+  "I couldn't help but overhear your comment about [topic]. Mind sharing more about that?",
   "You seem like you're having a good time. What's been the highlight of your night so far?",
   "The vibe here is chill. Do you know if they host any live music or special events?",
   "I just noticed that artwork on the wall—kind of intriguing, right?",
